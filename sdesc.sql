2380841e2b37d90ac76f5f98200b1250be53aae0
Rykov_D <drycov@gmail.com>
2020-04-13 16:08:22 +0600

    xxxx

--- sdesc.sql
+++ sdesc.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Апр 03 2020 г., 12:38
--- Версия сервера: 5.7.26
--- Версия PHP: 7.2.18
+-- Хост: 127.0.0.1
+-- Время создания: Апр 13 2020 г., 11:55
+-- Версия сервера: 10.4.11-MariaDB
+-- Версия PHP: 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -28,36 +27,41 @@ SET time_zone = "+00:00";
 -- Структура таблицы `activity`
 --
 
-DROP TABLE IF EXISTS `activity`;
-CREATE TABLE IF NOT EXISTS `activity` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `activity` (
+  `id` int(11) NOT NULL,
   `date` date NOT NULL,
   `user_id` int(11) NOT NULL,
   `title_event` varchar(50) DEFAULT NULL,
-  `event` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  `event` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `messages`
+-- Структура таблицы `checked_sms`
 --
 
-DROP TABLE IF EXISTS `messages`;
-CREATE TABLE IF NOT EXISTS `messages` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_to` int(11) NOT NULL,
-  `user_from` int(11) NOT NULL,
-  `subject` varchar(100) CHARACTER SET latin1 NOT NULL,
-  `message` text CHARACTER SET latin1 NOT NULL,
-  `respond` int(11) NOT NULL DEFAULT '0',
-  `sender_open` enum('y','n') CHARACTER SET latin1 NOT NULL DEFAULT 'y',
-  `receiver_open` enum('y','n') CHARACTER SET latin1 NOT NULL DEFAULT 'n',
-  `sender_delete` enum('y','n') CHARACTER SET latin1 NOT NULL DEFAULT 'n',
-  `receiver_delete` enum('y','n') CHARACTER SET latin1 NOT NULL DEFAULT 'n',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `checked_sms` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `sms_key` int(11) DEFAULT NULL,
+  `key_time` bigint(15) UNSIGNED DEFAULT NULL,
+  `user_ip` bigint(13) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `comments`
+--
+
+CREATE TABLE `comments` (
+  `comm_id` int(11) NOT NULL,
+  `ti_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `comm_text` varchar(255) DEFAULT NULL,
+  `comm_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -65,9 +69,8 @@ CREATE TABLE IF NOT EXISTS `messages` (
 -- Структура таблицы `personal_info`
 --
 
-DROP TABLE IF EXISTS `personal_info`;
-CREATE TABLE IF NOT EXISTS `personal_info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `personal_info` (
+  `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
   `f_name` varchar(200) DEFAULT NULL,
   `l_name` varchar(200) DEFAULT NULL,
@@ -78,17 +81,57 @@ CREATE TABLE IF NOT EXISTS `personal_info` (
   `tab_num` int(10) DEFAULT NULL,
   `company_post` varchar(200) DEFAULT NULL,
   `region` int(8) DEFAULT NULL,
-  `date_password` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `date_password` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `personal_info`
 --
 
 INSERT INTO `personal_info` (`id`, `user_id`, `f_name`, `l_name`, `m_name`, `inn`, `mobile_phone`, `ip`, `tab_num`, `company_post`, `region`, `date_password`) VALUES
-(1, 1, 'Денис', 'Рыков', 'Игорьевич', 951025351348, 8710515252, 3232235806, 16435, '16', 5006, '2020-03-16 09:35:32'),
-(2, 2, 'Георгий', 'Колмаков', 'Александрович', 951025351348, 8710515252, 3232235806, 16435, '16', 5006, '2020-03-16 09:35:32');
+(1, 1, 'Денис', 'Рыков', 'Игорьевич', 951025351348, 7710515252, 168430280, 16435, '16', 5006, '2020-03-16 09:35:32'),
+(2, 2, 'Мария', 'Рыкова', 'Александровна', 970723351689, 7764970723, 531172805, 16436, '16', 5006, '2020-03-16 09:35:32');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `pm`
+--
+
+CREATE TABLE `pm` (
+  `id` bigint(20) NOT NULL,
+  `id2` int(11) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `user1` bigint(20) NOT NULL,
+  `user2` bigint(20) NOT NULL,
+  `message` text NOT NULL,
+  `timestamp` int(10) NOT NULL,
+  `user1read` varchar(3) NOT NULL,
+  `user2read` varchar(3) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `role_name` varchar(50) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `roles`
+--
+
+INSERT INTO `roles` (`id`, `role_name`, `description`) VALUES
+(1, 'Пользователь', NULL),
+(2, 'Роль 2', NULL),
+(3, 'Роль 3', NULL),
+(4, 'Роль 4', NULL),
+(5, 'Администратор', NULL);
 
 -- --------------------------------------------------------
 
@@ -96,38 +139,26 @@ INSERT INTO `personal_info` (`id`, `user_id`, `f_name`, `l_name`, `m_name`, `inn
 -- Структура таблицы `tickets`
 --
 
-DROP TABLE IF EXISTS `tickets`;
-CREATE TABLE IF NOT EXISTS `tickets` (
-  `ti_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tickets` (
+  `ti_id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL DEFAULT '1',
-  `text` varchar(255) NOT NULL DEFAULT '1',
+  `text` varchar(255) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `owner_id` int(11) DEFAULT '1',
+  `owner_id` int(11) DEFAULT 1,
   `priority` int(1) DEFAULT NULL,
-  `status` int(1) DEFAULT '1',
+  `status` int(1) DEFAULT 1,
   `ti_accept` int(11) DEFAULT NULL,
   `ti_date` date DEFAULT NULL,
   `ti_email` varchar(50) DEFAULT NULL,
-  `ti_phone` varchar(16) DEFAULT NULL,
-  PRIMARY KEY (`ti_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+  `ti_phone` varchar(16) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `tickets`
 --
 
 INSERT INTO `tickets` (`ti_id`, `title`, `text`, `user_id`, `owner_id`, `priority`, `status`, `ti_accept`, `ti_date`, `ti_email`, `ti_phone`) VALUES
-(1, 'Проверка работы системы', 'В данном примере с помощью команды group by строки группируются по возрасту и для каждой группы через запятую выводятся имена работников с таким возрастом:', 1, 2, 1, 1, NULL, '2020-03-26', 'Rykov_D@zashita.railways.kz', '87233760300'),
-(2, 'Проверка работы системы', 'В данном примере с помощью команды group by строки группируются по возрасту и для каждой группы через запятую выводятся имена работников с таким возрастом:', 2, 1, 2, 2, NULL, '2020-03-26', 'Rykov_D@zashita.railways.kz', '87233760300'),
-(3, 'Проверка работы системы', 'В данном примере с помощью команды group by строки группируются по возрасту и для каждой группы через запятую выводятся имена работников с таким возрастом:', 2, 1, 5, 2, NULL, '2020-03-26', 'Rykov_D@zashita.railways.kz', '87233760300'),
-(4, 'Проверка работы системы', 'В данном примере с помощью команды group by строки группируются по возрасту и для каждой группы через запятую выводятся имена работников с таким возрастом:', 1, 2, 3, 3, NULL, '2020-03-26', 'Rykov_D@zashita.railways.kz', '87233760300'),
-(5, 'Проверка работы системы', 'В данном примере с помощью команды group by строки группируются по возрасту и для каждой группы через запятую выводятся имена работников с таким возрастом:', 2, 1, 4, 4, NULL, '2020-03-26', 'Rykov_D@zashita.railways.kz', '87233760300'),
-(6, 'Проверка работы системы', 'В данном примере с помощью команды group by строки группируются по возрасту и для каждой группы через запятую выводятся имена работников с таким возрастом:', 1, 2, 5, 1, NULL, '2020-03-26', 'Rykov_D@zashita.railways.kz', '87233760300'),
-(7, 'Проверка работы системы', 'В данном примере с помощью команды group by строки группируются по возрасту и для каждой группы через запятую выводятся имена работников с таким возрастом:', 2, 1, 1, 2, NULL, '2020-03-26', 'Rykov_D@zashita.railways.kz', '87233760300'),
-(8, 'Проверка работы системы', 'В данном примере с помощью команды group by строки группируются по возрасту и для каждой группы через запятую выводятся имена работников с таким возрастом:', 1, 2, 2, 3, NULL, '2020-03-26', 'Rykov_D@zashita.railways.kz', '87233760300'),
-(9, 'Проверка работы системы', 'В данном примере с помощью команды group by строки группируются по возрасту и для каждой группы через запятую выводятся имена работников с таким возрастом:', 2, 1, 3, 4, NULL, '2020-03-26', 'Rykov_D@zashita.railways.kz', '87233760300'),
-(10, 'Проверка работы системы', 'В данном примере с помощью команды group by строки группируются по возрасту и для каждой группы через запятую выводятся имена работников с таким возрастом:', 1, 2, 4, 1, NULL, '2020-03-26', 'Rykov_D@zashita.railways.kz', '87233760300'),
-(11, 'Проверка работы системы', 'В данном примере с помощью команды group by строки группируются по возрасту и для каждой группы через запятую выводятся имена работников с таким возрастом:', 1, 2, 1, 3, NULL, '2020-03-26', 'Rykov_D@zashita.railways.kz', '87233760300');
+(1, 'test', '', 1, 1, 1, 1, NULL, '2020-04-13', 'drycov@gmail.com', '7710515252');
 
 -- --------------------------------------------------------
 
@@ -135,30 +166,14 @@ INSERT INTO `tickets` (`ti_id`, `title`, `text`, `user_id`, `owner_id`, `priorit
 -- Структура таблицы `ticket_timeline`
 --
 
-DROP TABLE IF EXISTS `ticket_timeline`;
-CREATE TABLE IF NOT EXISTS `ticket_timeline` (
-  `tm_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `ticket_timeline` (
+  `tm_id` int(11) NOT NULL,
   `ti_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `event` varchar(255) NOT NULL,
   `event_id` int(11) DEFAULT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY (`tm_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `ticket_timeline`
---
-
-INSERT INTO `ticket_timeline` (`tm_id`, `ti_id`, `user_id`, `event`, `event_id`, `date`) VALUES
-(1, 1, 1, 'Создание', 1, '2020-01-15 12:25:20'),
-(2, 1, 3, 'Назначен исполнитель', 2, '2020-01-15 12:25:20'),
-(3, 2, 2, 'Создание', 1, '2020-01-15 12:33:41'),
-(4, 2, 3, 'Назначен исполнитель', 2, '2020-01-15 12:33:41'),
-(5, 1, 1, 'Тикет закрыт!', 5, '2020-01-15 14:45:49'),
-(6, 2, 1, 'Тикет закрыт!', 5, '2020-01-15 15:46:14'),
-(7, 1, 1, 'Тикет возобновлен!', 1, '2020-01-15 15:50:44'),
-(8, 1, 1, 'Тикет закрыт!', 5, '2020-01-15 15:50:48');
+  `date` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -166,49 +181,126 @@ INSERT INTO `ticket_timeline` (`tm_id`, `ti_id`, `user_id`, `event`, `event_id`,
 -- Структура таблицы `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(15) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(15) NOT NULL,
+  `username` varchar(200) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `status` int(1) DEFAULT NULL,
-  `role` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+  `role` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 --
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `status`, `role`) VALUES
-(1, 'Rykov_D@zashita.railways.kz', '$2y$10$GOmPZX.VWURKk6vkKSWIL.QDLT7vcKOLwN4sk02Wa8eW1zJTYGIBC', 1, '5'),
-(2, 'Kolmakov_G@ktzh.railways.kz', '$2y$10$GOmPZX.VWURKk6vkKSWIL.QDLT7vcKOLwN4sk02Wa8eW1zJTYGIBC', 1, '1');
-
--- --------------------------------------------------------
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `status`, `role`) VALUES
+(1, 'MRykova', 'DRycov@gmail.com', '$2y$10$GOmPZX.VWURKk6vkKSWIL.QDLT7vcKOLwN4sk02Wa8eW1zJTYGIBC', 1, '5'),
+(2, 'MRykova', 'MRykova@gmail.com', '$2y$10$GOmPZX.VWURKk6vkKSWIL.QDLT7vcKOLwN4sk02Wa8eW1zJTYGIBC', 1, '1');
 
 --
--- Структура таблицы `user_info`
+-- Индексы сохранённых таблиц
 --
 
-DROP TABLE IF EXISTS `user_info`;
-CREATE TABLE IF NOT EXISTS `user_info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `f_name` varchar(50) DEFAULT NULL,
-  `l_name` varchar(50) DEFAULT NULL,
-  `m_name` varchar(50) DEFAULT NULL,
-  `phone` text,
-  `ip` int(10) UNSIGNED DEFAULT NULL,
-  `date_password` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+--
+-- Индексы таблицы `activity`
+--
+ALTER TABLE `activity`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Дамп данных таблицы `user_info`
+-- Индексы таблицы `checked_sms`
+--
+ALTER TABLE `checked_sms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comm_id`);
+
+--
+-- Индексы таблицы `personal_info`
+--
+ALTER TABLE `personal_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `tickets`
+--
+ALTER TABLE `tickets`
+  ADD PRIMARY KEY (`ti_id`);
+
+--
+-- Индексы таблицы `ticket_timeline`
+--
+ALTER TABLE `ticket_timeline`
+  ADD PRIMARY KEY (`tm_id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
 --
 
-INSERT INTO `user_info` (`id`, `user_id`, `f_name`, `l_name`, `m_name`, `phone`, `ip`, `date_password`) VALUES
-(1, 1, 'Денис', 'Рыков', 'Игорьевич', '+77710515252', 4294967295, '2020-03-16 09:35:32');
+--
+-- AUTO_INCREMENT для таблицы `activity`
+--
+ALTER TABLE `activity`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `checked_sms`
+--
+ALTER TABLE `checked_sms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comm_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `personal_info`
+--
+ALTER TABLE `personal_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `tickets`
+--
+ALTER TABLE `tickets`
+  MODIFY `ti_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `ticket_timeline`
+--
+ALTER TABLE `ticket_timeline`
+  MODIFY `tm_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
